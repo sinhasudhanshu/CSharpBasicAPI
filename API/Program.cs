b2359f7fbe78938ad0463f46f94696d3a3a60ebf
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2023-07-18 16:06:14 +0530

    Adding DBContext in program.cs
    here we are requesting connection to connect sqlite data base

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<LibraryContext>(opt =>
+
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("Default Connection"));
+}
+);
 
 var app = builder.Build();
 
