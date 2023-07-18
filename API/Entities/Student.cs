5177a95a3e6aa42174161c10b55e7ec99cd769c7
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2023-07-18 15:31:54 +0530

    created a entitie for controller
    where we define Id name subject number

---
+++ API/Entities/Student.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public int Number { get; set; }
+    }
+}
